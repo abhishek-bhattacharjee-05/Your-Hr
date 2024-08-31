e2613c4a17161c7811f5403c2fb3d5d4e8edea88
abhishekcodes05 <abhishekbhattacharjee200@gmail.com>
2024-09-01 02:48:29 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -6,15 +6,19 @@ const cors = require('cors');
 dotenv.config();
 const app = express();
 
-
 app.use(express.json());
 app.use(cors());
 app.use('/uploads', express.static('uploads'));
+
+// Add a route for the root URL
+app.get('/', (req, res) => {
+  res.send('Welcome to the API!');
+});
 
 app.use('/api/users', require('./routes/userRoutes'));
 
 const PORT = process.env.PORT || 3000;
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`)))
+  .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
   .catch(err => console.log(err));
